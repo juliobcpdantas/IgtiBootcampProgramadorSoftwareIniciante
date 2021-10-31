a744d8c7382bcf202f79ac81a2b11426e1874b6c
juliobcpdantas <juliobcpdantas@hotmail.com>
2021-10-31 18:01:56 -0300

    atualizando repositório M3

---
+++ Módulo 3 - Programador(a) de Software Iniciante/Aulas de Python.py
@@ -0,0 +1,25 @@
+# Aula de Python 
+
+# tipos de dados
+'''
+String
+Integer
+Float
+Boolean
+'''
+
+e = 2e3 # exponencial 10
+print(type(e))
+print(e)
+
+# list [] ordenada e mutável, pode dados duplicados
+# tuple () ordenada e imutável, pode dados duplicados
+# set {} ordenada e desindexada, sem dados duplicados
+# Dictionary {'a':'aa', 'b':'bb', 'c':'cc'} desordenada e mutável, sem dados duplicados
+
+listaDeCompras = ['a', 'b', 'c', 'd']
+print(listaDeCompras)
+
+#listas (Arrays)
+frutas = ['banana', 'laranja', 'abacate', 'melancia', 'caju', 'abacaxi']
+saldoAlunos = [500.00, 1200.00]
