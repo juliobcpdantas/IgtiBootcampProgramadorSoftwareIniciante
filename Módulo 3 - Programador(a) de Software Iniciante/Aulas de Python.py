cef8261b5b770c2aba07454b8cf9cb6c9d8830ea
juliobcpdantas <juliobcpdantas@hotmail.com>
2021-11-01 13:25:01 -0300

    atualizando repositório M3 01.11.21

--- Módulo 3 - Programador(a) de Software Iniciante/Aulas de Python.py
+++ Módulo 3 - Programador(a) de Software Iniciante/Aulas de Python.py
@@ -20,6 +20,132 @@ print(e)
 listaDeCompras = ['a', 'b', 'c', 'd']
 print(listaDeCompras)
 
-#listas (Arrays)
+# list (Arrays) * Trab. Prático
 frutas = ['banana', 'laranja', 'abacate', 'melancia', 'caju', 'abacaxi']
-saldoAlunos = [500.00, 1200.00]
+saldoAlunos = [500.00, 1200.00, 1000.00, 600.00, 50000.00]
+numPaciente = [25, 35, 50, 45, 22, 33, 89]
+
+print(frutas)
+print(saldoAlunos)
+print(numPaciente)
+
+
+frutasDuplic = ['banana', 'laranja', 'abacate', 'melancia', 'caju', 'abacaxi', 'banana', 'laranja', 'abacate', 'melancia', 'caju', 'abacaxi']
+print(frutasDuplic)
+
+print(len(saldoAlunos), len(numPaciente), len(numPaciente),)
+
+print(frutas[0], frutas[5])
+
+frutas.append('morangos')
+print(frutas)
+
+saldoAlunos.extend(numPaciente)
+print("saldoAlunos + numPaciente: ", saldoAlunos)
+
+# tuples
+
+frutas2 = ('banana', 'laranja', 'abacate', 'melancia', 'caju', 'abacaxi')
+saldoAlunos2 = (500.00, 1200.00, 1000.00, 600.00, 50000.00)
+numPaciente2 = (25, 35, 50, 45, 22, 33, 89)
+
+print(frutas2[1])
+
+print(frutas2[1:4])
+
+tupleJuntos = frutas2 + numPaciente2
+print(tupleJuntos)
+
+print(type(tupleJuntos))
+
+numPacientex = (25, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89,
+35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 
+35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 
+35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 35, 50, 45, 22, 33, 89, 
+35, 50, 45, 22, 33, 89)
+
+print(numPacientex.count(89))
+
+# sets
+
+cidades = {'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal'}
+print(type(cidades))
+print(cidades)
+
+cidadeRepeat = {'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal',
+'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal',
+'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal',
+'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal',
+'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal',
+'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal',
+'Belo Horizonte', 'Manaus', 'Fortaleza', 'Natal'}
+
+print(cidadeRepeat)
+
+print('Manaus' in cidades)
+print('Recife' in cidades)
+
+cidades.add('Porto Alegre')
+print(cidades)
+
+cidades2 = {'Recife', 'Salvador', 'Campo Grande'}
+cidades.update(cidades2)
+
+print(cidades)
+
+# Dictionary
+
+codUf = {
+    21 : 'Maranhão',
+    22 : 'Piauí',
+    23 : 'Ceará',
+    24 : 'Rio Grande do Norte',
+    25 : 'Paraíba',
+    26 : 'Pernambuco',
+    27 : 'Alagoas',
+    28 : 'Sergipe',
+    29 : 'Bahia'
+}
+
+
+print(type(codUf))
+
+print(codUf)
+
+
+print(codUf.values())
+
+codUfDuplic = {
+    21 : 'Maranhão',
+    22 : 'Piauí',
+    23 : 'Ceará',
+    24 : 'Rio Grande do Norte',
+    25 : 'Paraíba',
+    26 : 'Pernambuco',
+    27 : 'Alagoas',
+    28 : 'Sergipe',
+    29 : 'Bahia',
+    21 : 'Maranhão',
+    22 : 'Piauí',
+    23 : 'Ceará',
+    24 : 'Rio Grande do Norte',
+    25 : 'Paraíba',
+    26 : 'Pernambuco',
+    27 : 'Alagoas',
+    28 : 'Sergipe',
+    29 : 'Bahia'
+}
+
+print(codUfDuplic)
+
+print(
+    codUf.get(22)
+)
+
+print(
+    codUf.keys()
+)
+
+codUf[30] = 'Estado Vira-lata Caramelo'
+
+print(codUf)
---
+++ Módulo 3 - Programador(a) de Software Iniciante/TrabalhoPraticoM03/CodTrabPratico03.py
@@ -0,0 +1,48 @@
+# Trabalho Prático Modulo 03 - BootCamp IGTI
+
+listaCompras = ['leite em pó', 'mamão', 'queijo']
+# print(
+#     type(listaCompras)
+# )
+
+idClientes2 = (100, 125, 478, 547, 565)
+# print(
+#     type(idClientes2)
+# )
+
+codUf = {'mg' : 30, 'sp' : 35}
+# print(
+#     type(codUf)
+# )
+
+pessoa = 'Marcelo'
+# print(
+# type(pessoa)
+# )
+
+numPessoas = 274
+# print(
+# type(numPessoas)
+# )
+
+distKm = 25.23
+# print(
+# type(distKm)
+# )
+
+custoCarro = '500,00'
+# print(
+# type(custoCarro)
+# )
+
+capitaisSulBr = ['Porto Alegre', 'Curitiba', 'Florianópolis']
+# print(
+# type(capitaisSulBr)
+# )
+# print(capitaisSulBr)
+
+
+aluno = 'julio de Barros Carvalho'
+# print(aluno)
+
+
